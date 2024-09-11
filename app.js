2218fa386cc7918515f4c3a6f529d14ad624f9f7
kimji <jhkim@youngjintech.com>
2024-09-11 17:30:39 +0900

    feat : 파일업로드 -> DB 경로 삽입 & 서버 -> 클라이언트 파일 내보내기

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const userController = require('./controller/userController');
+const fileController = require('./controller/fileController');
+
+const multer = require('multer');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const express = require('express');
 
@@ -8,11 +12,39 @@ const port = 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+//파일 저장위치 및 파일명 설정 
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, 'uploads/'); //저장할 폴더
+    },
+    filename: function(req, file, cb){
+        const uniqueSuffix = Date.now()+'-'+Math.round(Math.random() * 169);
+        cb(null, uniqueSuffix+'-'+file.originalname); //고유한 파일명 생성
+    }
+});
+
+
+const upload = multer({storage: storage});
+
+//업로드 폴더가 없으면 생성
+if(!fs.existsSync('./uploads')){
+    fs.mkdirSync('./uploads');
+}
+
 app.post('/register', userController.Register);
 app.post('/login', userController.Login);
 app.post('/refresh_token', userController.refreshJWT);
 
 app.get('/sample', userController.authenticateToken ,userController.SampleAPI);
+
+
+app.post('/upload', upload.single('file'), fileController.filesUpload);
+app.get('/file/:sys_file_id', fileController.filesShowClient);
+
+
+
+
 
 app.listen(port, ()=> {
     console.log(`Node_Server API listening on Port : ${port}`);
